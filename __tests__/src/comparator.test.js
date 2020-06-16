aa796555834d58cf0018fdb5f95f2b0796183811
Andrey Solovyev <solovyev833@gmail.com>
2020-06-16 13:11:34 +0300

    Test workflow (12)

--- __tests__/src/comparator.test.js
+++ __tests__/src/comparator.test.js
@@ -1,11 +1,13 @@
 import { test, expect } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
-import compareFiles from '../../src/comparator';
+import compareFiles from '../../src/comparator.js';
 
 test('Gendiff', () => {
-  const before = path.join(__dirname, '..', '..', '__fixtures__', 'before.json');
-  const after = path.join(__dirname, '..', '..', '__fixtures__', 'after.json');
-  const result = fs.readFileSync(`${__dirname}/../../__fixtures__/result.txt`, 'utf8');
+  const currentDir = process.cwd();
+  const before = path.resolve(currentDir, '__fixtures__', 'before.json');
+  const after = path.resolve(currentDir, '__fixtures__', 'after.json');
+  const resultPath = path.resolve(currentDir, '__fixtures__', 'result.txt');
+  const result = fs.readFileSync(resultPath, 'utf8');
   expect(compareFiles(before, after)).toBe(result);
 });
